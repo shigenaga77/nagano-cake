97db2ccd26a09d68fd334788119577aa27771b7e
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2023-02-07 14:52:42 +0000

    [add]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,6 +14,20 @@ class Admin::ItemsController < ApplicationController
         @items = Item.all
     end
     
+    def show
+        @item = Item.find(params[:id])
+    end
+    
+    def edit
+        @item = Item.find(params[:id])
+    end
+    
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to admin_items_path
+    end
+
     private
     
     def item_params
